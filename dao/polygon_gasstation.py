09c4b660b5ff8d094674fd4939718556c4aa93ff
Sergey-Kishkurno <54913946+Sergey-Kishkurno@users.noreply.github.com>
2021-08-16 23:00:30 +0300

    Add files via upload

---
+++ dao/polygon_gasstation.py
@@ -0,0 +1,11 @@
+from sources import save_file
+
+
+save_file(
+    [
+        ("safeLow",     "standard",     "fast", "fastest",      "blockTime",    "blockNumber"),
+        (6.000000002,   6.000000002,    9,      50,             2,              17806701),
+        (6.000000002,   6.000000002,    6,      60.00000002,    2,              17806704),
+        (5,             5,              5,      16.885200002,   2,              17806709),
+    ]
+)
